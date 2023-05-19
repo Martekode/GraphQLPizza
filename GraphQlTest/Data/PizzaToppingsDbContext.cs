d35ea2b152e4433d8191eebad1c5e7d0ca38a8d0
Martekode <BrianDesmedt9000@hotmail.com>
2023-05-19 14:19:33 +0200

    added dbcontext for databse

---
+++ GraphQlTest/Data/PizzaToppingsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GraphQlTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQlTest.Data
+{
+    public class PizzaToppingsDbContext : DbContext
+    {
+        public PizzaToppingsDbContext(DbContextOptions<PizzaToppingsDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<PizzaType> Pizzas { get; set; }
+        public DbSet<ToppingType> Toppings { get; set; }
+    }
+}
