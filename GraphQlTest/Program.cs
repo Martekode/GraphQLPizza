72ed56bc9062178f9ca03f592d18ef56ce33003d
Martekode <BrianDesmedt9000@hotmail.com>
2023-05-19 14:24:09 +0200

    added support for SQL server

--- GraphQlTest/Program.cs
+++ GraphQlTest/Program.cs
@@ -1,9 +1,11 @@
+using GraphQlTest.Data;
+using GraphQlTest.Exeptions;
 using GraphQlTest.Schema.Mutations;
 using GraphQlTest.Schema.Querries;
 using GraphQlTest.Schema.Subscriptions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 //services and config 
 builder.Services
@@ -12,6 +14,12 @@ builder.Services
     .AddQueryType<Query>()
     .AddMutationType<Mutation>()
     .AddSubscriptionType<Subscription>();
+
+builder.Services.AddSingleton<IErrorFilter, NotFoundErrorFilter>();
+builder.Services.AddPooledDbContextFactory<PizzaToppingsDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+});
 
 // app building and USES
 var app = builder.Build();
