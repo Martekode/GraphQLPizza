c395f470844afc1db8cd640df0d0bf7c789a87fb
Martekode <BrianDesmedt9000@hotmail.com>
2023-05-19 14:20:15 +0200

    tried addind custom exeption handler, failed

---
+++ GraphQlTest/Exeptions/NotFoundErrorFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace GraphQlTest.Exeptions
+{
+    public class NotFoundErrorFilter : IErrorFilter
+    {
+        public IError OnError(IError error)
+        {
+            if(error is NotFoundExeption)
+            {
+                var message = "Not Found";
+                var extensions = new Dictionary<string, object>
+                {
+                    {"code", "NOT_FOUND" }
+                };
+
+                return error.WithMessage(message).WithExtensions(extensions);
+            }
+            return error;
+        }
+    }
+}
---
+++ GraphQlTest/Exeptions/NotFoundExeption.cs
@@ -0,0 +1,11 @@
+﻿using HotChocolate.Execution;
+
+namespace GraphQlTest.Exeptions
+{
+    public class NotFoundExeption : QueryException
+    {
+        public NotFoundExeption(string message) : base(message) 
+        { 
+        }
+    }
+}
